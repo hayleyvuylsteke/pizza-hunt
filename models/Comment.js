11db3508bcab6dd956319a891f8529da474170a0
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-04-07 20:39:53 -0400

    pushing code

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  comments: [
+    {
+      type: String,
+    }
+  ]
+});
+
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
